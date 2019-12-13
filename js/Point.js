bf77130945373ef2361d5af838f4dee658b6dfce
freakleon <531085259@qq.com>
2019-12-13 00:02:46 -0600

    fuck

--- js/Point.js
+++ js/Point.js
@@ -1,117 +1,47 @@
-﻿function Point(initCoords, attr) {
-	this.className = "Point";
-	var coords = [];
-	coords[0] = initCoords[0] + (Math.random() - 0.5) / 1000;
-	coords[1] = initCoords[1] + (Math.random() - 0.5) / 1000;
-	this.jxgPoint = null;
-	this.attr = {
-		fillColor: "black",
-		strokeColor: "black",
-		withLabel: false
-	};
-	this.setAttribute(attr);
-	this.id = objectId++;
-
-	Object.defineProperties(this, {
-		"coords": {
-			"get": function () {
-				var updatedCoords;
-				if (this.jxgPoint != null) {
-					updatedCoords = this.jxgPoint.coords.usrCoords.slice(1, 3);
-					var i;
-					for (i = 0; i < updatedCoords.length; i++) {
-						updatedCoords[i] = updatedCoords[i] / this.jxgPoint.coords.usrCoords[0];
-					}
-					coords = updatedCoords;
-					return updatedCoords;
-				}
-				return coords;
-			},
-			"set": function (newCoords) {
-				if (this.jxgPoint) {
-					this.jxgPoint.setPositionDirectly(JXG.COORDS_BY_USER, [1, newCoords[0], newCoords[1]], this.jxgPoint.coords.usrCoords);
-					coords = this.jxgPoint.coords.usrCoords.slice(1);
-				}
-				else
-					coords = newCoords;
-			}
-		},
-
-		"x": {
-			"get": function () {
-				return this.coords[0];
-			},
-			"set": function (newX) {
-				this.coords = [newX, coords[1]];
-			}
-		},
-
-		"y": {
-			"get": function () {
-				return this.coords[1];
-			},
-			"set": function (newY) {
-				this.coords = [coords[0], newY];
-			}
-		}
-
-	});
-
-
-
-	this.toJSON = function () {
-		return {coords: [this.x, this.y],attr: this.attr,id: this.id, className: this.className};
+﻿class point {
+	constructor(x, y, isSite,points) {
+	  this.x = x;
+	  this.y = y;
+	  this.isSite = isSite;
+	  this.points = points;
 	}
-}
 
-Point.prototype.setAttribute = function (attr) {
-	Object.assign(this.attr, attr);
-	if (this.jxgPoint)
-		this.jxgPoint.setAttribute(this.attr);
-}
+	addcenter(p){
+		if(this.isSite){
+			this.points.push(p);
+		}
+	}
 
-//this will not clone the JXG portion of the point, but will maintain all data defined in this object
-Point.prototype.clone = function () {
-	var attr = {};
-	Object.assign(attr, this.attr);
-	return new Point([this.coords[0], this.coords[1]], attr);
-}
+	static center(p1, p2, p3) {
+		var x1 = p1.x, y1 = p1.y, x2 = p2.x, y2 = p2.y, x3 = p3.x, y3 = p3.y;
+		var a, b;
+		a = (y2 - y1) / (x2 - x1);
+		b = y1 - a * x1;
+	
+		var xMiddle = (x1 + x2) / 2;
+		var yMiddle = (y1 + y2) / 2;
+		var c, lastX, lastY;
+		if (a != 0) {
+			c = yMiddle - (-1 / a) * xMiddle;
+			lastX = (Math.pow(x1, 2) + Math.pow(y1, 2) - Math.pow(x3, 2) - Math.pow(y3, 2) - 2 * c * y1 + 2 * c * y3) / (2 * ((x1 - x3) - (1 / a) * (y1 - y3)));
+			lastY = (-1 / a) * lastX + c;
+		} else {
+			lastX = c = xMiddle;
+			lastY = (Math.pow(x1, 2) + Math.pow(y1, 2) - Math.pow(x3, 2) - Math.pow(y3, 2) + 2 * lastX * (x3 - x1)) / (2 * (y1 - y3));
+		}
+		var points = [];
+		points.push(p1,p2,p3);
+		return new point(lastX,lastY,false,points);
+	}
 
-Point.prototype.isNear = function (p) {
-	return (Math.abs(this.x - p.x) < 0.001) && (Math.abs(this.y - p.y) < 0.001);
-};
+	static distance(a, b) {
+		const dx = a.x - b.x;
+		const dy = a.y - b.y;
+	
+		return Math.hypot(dx, dy);
+	}
 
-Point.compareX = function (p1, p2) {
-	return p1.coords[0] - p2.coords[0];
-};
-
-Point.compareY = function (p1, p2) {
-	return p1.coords[1] - p2.coords[1];
-};
-
-Point.orient = function (a, b, c) {
-	return (b.coords[0] - a.coords[0]) * (c.coords[1] - a.coords[1]) -
-		(c.coords[0] - a.coords[0]) * (b.coords[1] - a.coords[1]);
-};
-
-Point.samePoint = function (p1, p2) {
-	return p1.x == p2.x && p1.y == p2.y;
-};
-
-Point.add = function (p1, p2) {
-	return new Point([p1.x + p2.x, p1.y + p2.y]);
-}
-
-Point.getSlope = function (p1, p2) {
-	return (p1.y - p2.y) / (p1.x - p2.x);
-};
-
-Point.prototype.divideByScalar = function (scalar) {
-	var x = this.x / scalar;
-	var y = this.y / scalar;
-
-	var newPoint = this.clone();
-	newPoint.x = x;
-	newPoint.y = y;
-	return newPoint;
+	static isSamePoint(a, b){
+		return a.x == b.x && a.y == b.y;
+	}
 }
--- js/test.js
+++ js/test.js
@@ -1,14 +1,53 @@
-import RbTree from "../node_modules/red-black-tree-js"
+import RbTree from "../node_modules/red-black-tree-js/src/rbTree.js"
 
 
-const rbTree = new RbTree();
+var rbTree = new RbTree();
+var i;
 rbTree.insert(1, "foo");
-rbTree.insert(2, "bar");
-rbTree.insert(3, "bar");
-rbTree.insert(4, "bar");
-rbTree.insert(5, "bar");
-rbTree.insert(6, "bar");
-rbTree.remove(6);
+for(i=2;i<=6;i++){
+    rbTree.insert(i, "bar");
+    // console.log(rbTree.size);
+}
+var arr = rbTree.toSortedArray();
+// console.log(BS(arr,0,arr.length,3));
+function BS(arr,left,right,value){
+    if(left == right){
+        return neighbor(arr,left,arr.length);
+    }
+    var mid = left+Math.floor((right-left)/2);
+    
+    if(arr[mid].key == value){
+        return neighbor(arr,mid,arr.length);
+    }
+    if(arr[mid].key < value){
+        return BS(arr,mid+1,right,value);
+    }else{
+        return BS(arr,left,mid,value);
+    }
+}
+function neighbor(array,mid,length){
+    var i;
+    var arr = [];
+    for(i=-2;i<=2;i++){
+        var j = mid+i;
+        if(j>=0 && j<length){
+            arr.push(array[j].value);
+        }
+    }
+    return arr;
+}
+var p1 = new point(116.3, 39.9,true);
+var p2 = new point(115.8, 39.8,true);
+var p3 = new point(117, 39.5,true);
+var c = point.center(p1,p2,p3);
+console.log(point.distance(p1,c));
+console.log(point.distance(p2,c));
+console.log(point.distance(p3,c));
+
+for(i=1;i<=6;i++){
+    rbTree.remove(i);
+    // console.log(rbTree.size);
+}
 
 const iterator = rbTree.createIterator();
 
